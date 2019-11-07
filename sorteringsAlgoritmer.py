7ca0c4b069e85a7b29b5b88bdd5be414d484af26
Chongsenpai <37104800+Chongsenpai@users.noreply.github.com>
2019-11-07 11:03:40 +0100

    I am sewer-king!" said Rat. "I am quick and cunning and I know every tunnel."

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -11,10 +11,20 @@ def insertionSort():
     importFile("testfiles/testcase0.txt")
     pass
 
-def bubbleSort():
-    importFile("testfiles/testcase0.txt")
-    pass
+def bubbleSort(list):
+    bubbleList = list.copy()
+    x = 0
+    for e in range(len(bubbleList) - 1):
+        x = x + 1
 
+        for i in range(len(bubbleList) - x):
+
+            if bubbleList[i] > bubbleList[i+1]:
+                bubbleList[i], bubbleList[i+1] = bubbleList[i+1], bubbleList[i]
+    return bubbleList
 
 if __name__ == "__main__":
     insertionSort()
+
+    L = [4, 3, 2, 1]
+    print(bubbleSort(L))
