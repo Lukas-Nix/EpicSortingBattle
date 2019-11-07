e1e0860bd871a54d700d458c04d7e1a43ffe3718
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-11-07 10:58:22 +0100

    Timer føles som dage

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,9 +1,18 @@
-#I vores insertionSort giver vi den en parameter "file", så at når vi kalder på funktionen, kræver den en parameter
+#I vores insertionSort giver vi den en parameter "list", så at når vi kalder på funktionen, kræver den en parameter
 def insertionSort(list):
-    sejereList = list.copy()
-    #Vi laver et for loop som kigger på elementerne mellem 0 og længden af elementer i listen "file"
-    for i in range(1,len(file)):
-        key=file[i]
+    #vi gemmer parameteren, list, som sin egen liste
+    sejereListe = list
+    #Vi laver et for loop som kigger på elementerne mellem 1, det andet element i listen, og mængden af elementer i listen "sejereListe"
+    for i in range(1, len(sejereListe)):
+        key = sejereListe[i]
+
+        j = i - 1
+        while j >= 0 and key < sejereListe[j]:
+            sejereListe[j + 1] = sejereListe[j]
+            j -= 1
+        sejereListe[j + 1] = key
+    return sejereListe
+
 
 def bubbleSort():
     importFile("testfiles/testcase0.txt")
@@ -11,4 +20,8 @@ def bubbleSort():
 
 
 if __name__ == "__main__":
-    insertionSort()
+    joy=[22,5,1,7,2,7,98,3]
+    insertionSort(joy)
+    #print(sejereListe)
+    for i in range(len(joy)):
+        print("% d" % joy[i])
