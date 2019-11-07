0c2d4cb42667bacc504a170cca046678260998f5
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-11-07 09:51:48 +0100

    Som blomster frø

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,5 +1,19 @@
+import matplotlib
+
+def importFile(filename):
+    outList = []
+    fil = open(filename)
+    for line in fil:
+        outList.append(line.strip())
+    return outList
+
 def insertionSort():
+    importFile("testfiles/testcase0.txt")
     pass
 
 def bubbleSort():
     pass
+
+
+if __name__ == "__main__":
+    insertionSort()
