6305a156b54f25c4fa9c2328a127735b2da657d2
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-11-07 10:19:32 +0100

    Hun forstår slet ingenting

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,12 +1,6 @@
-def importFile(filename):
-    outList = []
-    fil = open(filename)
-    for line in fil:
-        outList.append(line.strip())
-    return outList
-
 #I vores insertionSort giver vi den en parameter "file", så at når vi kalder på funktionen, kræver den en parameter
-def insertionSort(file):
+def insertionSort(list):
+    sejereList = list.copy()
     #Vi laver et for loop som kigger på elementerne mellem 0 og længden af elementer i listen "file"
     for i in range(1,len(file)):
         key=file[i]
