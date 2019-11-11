2381630ba4eca4e56180695cce4ce9c0fa4d9af6
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-11-11 15:08:45 +0100

    Til en verden af i går

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -3,13 +3,21 @@ def insertionSort(list):
     #vi gemmer parameteren, list, som sin egen liste
     sejereListe = list
     #Vi laver et for loop som bliver kørt igennem et antal gange lig med størrelsen på listen "sejereListe"
-    for i in range(0, len(sejereListe)):
-
+    for i in range(1, len(sejereListe)):
         #Igennem hvert loop bliver der defineret "key" til at være det nye element som den er nået til i listen.
         key = sejereListe[i]
 
-        #
+        #Efter det gemmer den så elementet før det nuværende element som sit eget element
         j = i - 1
+
+        '''
+        I hvert loop har den så den nuværende element og elementet før det element der arbejdes på,
+        ved at j er et tal mindre end i, altså et element før
+
+
+        Nu skal koden begynde at sortere.
+        I det følgende loop vil den tjekke om det originalle element er mindre end elementet før, og hvis det er, så bytter den plads på dem.
+        '''
         while j >= 0 and key < sejereListe[j]:
             sejereListe[j + 1] = sejereListe[j]
             j -= 1
@@ -23,7 +31,7 @@ def bubbleSort(list):
     # x er mængden af gange løkken er blevet kørt igennem #
     x = 0
 
-    # 
+    #
     for e in range(len(bubbleList) - 1):
         x = x + 1
 
@@ -35,15 +43,10 @@ def bubbleSort(list):
     return bubbleList
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    insertionSort()
-
     L = [4, 3, 2, 1]
     print(bubbleSort(L))
-=======
-    joy=[22,5,1,7,2,7,98,3]
+    joy=[22,5,1,7,2,7,98,3,22,5,1,7,2,7,98,3,22,5,1,7,2,7,98,3,22,5,1,7,2,7,98,3,22,5,1,7,2,7,98,3,22,5,1,7,2,7,98,3]
     insertionSort(joy)
     #print(sejereListe)
     for i in range(len(joy)):
         print("% d" % joy[i])
->>>>>>> 6f4336dfe3f89d483bd69de355e30eef0511c7b1
